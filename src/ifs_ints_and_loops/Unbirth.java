bff06e10423f8a59085894f4331a2441e2b4d6f9
Caden <league@WTS5.attlocal.net>
2018-10-07 15:58:31 -0700

    Unbirthday

---
+++ src/ifs_ints_and_loops/Unbirth.java
@@ -0,0 +1,12 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirth {
+public static void main(String[] args) {
+	String message = JOptionPane.showInputDialog(null, "What is your Birthday?");
+	if(message.equals("10/7")) {
+	JOptionPane.showMessageDialog(null, "Hab a noice dai");
+	}
+	}
+}
