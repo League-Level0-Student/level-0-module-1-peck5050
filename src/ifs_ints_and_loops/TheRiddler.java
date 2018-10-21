0bf9d875b5e32b96cd91acdddb1ca41a8efe26c5
1862386 <league@WTS5.attlocal.net>
2018-10-21 15:57:47 -0700

    riddler in progresso

--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -5,24 +5,36 @@ import javax.swing.JOptionPane;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
-
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
+		
 int score = 0;
+
+		//2. Make a pop up to show the score.
+
+JOptionPane.showMessageDialog(null, "Score 0");
+		
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-JOptionPane.showInputDialog("If roses are red and violets are blue, than wtf is the color violet?");
+		
+String purple = JOptionPane.showInputDialog("If roses are red and violets are blue, than wtf is the color violet?");
 
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-JOptionPane.showMessageDialog(null, "Nicu!");
+
+if(purple.equalsIgnoreCase("violet")){
+	JOptionPane.showMessageDialog(null, "Nice!");
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
+else {
+	JOptionPane.showMessageDialog(null, "WWWRRRROOOONNNNNGGGGGG");
+	
+}
 		// 6. Add some more riddles
 
-		// 2. Make a pop up to show the score.
+JOptionPane.showMessageDialog(null, "why did the honey badger cross the road? To tear the dudes face off");
 		
 	}
 }
